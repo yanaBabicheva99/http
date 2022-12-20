e633c19135953657e4eed01447bc8ee4f9b20103
yanaBabicheva99 <103054110+yanaBabicheva99@users.noreply.github.com>
2022-12-20 10:18:34 +0300

    creating project

---
+++ notes.controller.js
@@ -0,0 +1,43 @@
+const fs = require('fs/promises')
+const path = require('path')
+const chalk = require('chalk')
+
+const notesPath = path.join(__dirname, 'db.json')
+
+async function addNote(title) {
+
+  const notes = await getNotes()
+
+  const note = {
+    title,
+    id: Date.now().toString()
+  }
+
+  notes.push(note)
+
+  await fs.writeFile(notesPath, JSON.stringify(notes))
+  console.log(chalk.bgGreen('Note was added!'))
+}
+
+async function removeNote(id) {
+  const notes = await getNotes()
+  await fs.writeFile(notesPath, JSON.stringify(notes.filter(note => note.id !== id.toString())))
+  console.log(chalk.bgGreen('Note was removed!'))
+}
+async function getNotes() {
+  const notes = await fs.readFile(notesPath, {encoding: 'utf-8'})
+  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : []
+}
+async function printNotes() {
+  const notes = await getNotes()
+
+  console.log(chalk.bgBlue('Here is the list of notes'))
+  notes.forEach(note => {
+    console.log(chalk.blue( `${note.id}  ${note.title}`))
+  })
+}
+module.exports = {
+  addNote,
+  printNotes,
+  removeNote
+}
